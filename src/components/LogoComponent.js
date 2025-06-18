fbf6b320168c873c3994c9f379be2360c3ab5c31
MUSTAFA892 <mustafatinwala6@gmail.com>
2025-06-18 18:14:47 +0530

    fixed the logo component

--- src/components/LogoComponent.js
+++ src/components/LogoComponent.js
@@ -5,15 +5,28 @@ import styled from 'styled-components'
 
 
 const Logo = styled.h1`
-display: inline-block;
-color: #c569f0;
-font-family: 'Pacifico',cursive;
+  display: inline-block;
+  color: #c569f0;
+  font-family: 'Pacifico', cursive;
 
-position: fixed;
-left: 2rem;
-top: 2rem;
-z-index:3;
-`
+  position: fixed;
+  left: 2rem;
+  top: 0.5rem; 
+  z-index: 3;
+
+  @media (max-width: 768px) {
+    left: 1rem;
+    top: 1rem;
+    font-size: 2.5rem;
+  }
+
+  @media (max-width: 480px) {
+    left: 0.8rem;
+    top: 1rem;
+    font-size: 2.5rem;
+  }
+`;
+
 
 const LogoComponent = (props) => {
     return (
